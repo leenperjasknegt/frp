53ad7e79467550239a904e38b5d9ba7337b8ec57
leenperjasknegt <leenknegtjasper@gmail.com>
2022-11-28 19:23:31 +0100

    Update and rename future_update.sh to to_do.sh

--- to_do.sh
+++ to_do.sh
@@ -5,3 +5,8 @@ varmac=$(ip -o link show dev eth0 | grep -Po 'ether \K[^ ]*' | sed 's/://g')
 
 sed -i "12c\[$varmac]" /opt/frpc/frpc.ini
 sed -i "16c\subdomain = $varmac" /opt/frpc/frpc.ini
+
+
+
+#Find used network interface: route | grep '^default' | grep -o '[^ ]*$'
+#Print MAC Address from used network interface: ip -o link show dev enp3s0| grep -Po 'ether \K[^ ]*'
